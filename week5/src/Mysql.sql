89e9dd5a335918d9eb83c3963a811206e2a035d5
NFTY19 <2429325436@qq.com>
2021-03-12 15:47:53 +0800

    MySQL练习题15-29

--- week5/src/Mysql.sql
+++ week5/src/Mysql.sql
@@ -101,3 +101,81 @@ INSERT INTO test_temp(NAME, age, score, address) VALUES
 
 #14
 INSERT INTO test SELECT * FROM test_temp;
+
+#15
+UPDATE test SET score = score + 5 WHERE age <= 20;
+
+#16
+UPDATE test SET age = age - 1 WHERE address LIKE "%南福苑%";
+
+#17
+DELETE FROM test
+WHERE age > 21 AND score >= 500;
+
+#18
+DELETE FROM test
+WHERE score<550 AND address LIKE '%重邮宿舍%';
+
+#19
+CREATE TABLE Student(
+	SNO VARCHAR(20),
+	NAME VARCHAR(10),
+	Age INTEGER,
+	College VARCHAR(30)
+);
+
+#20
+CREATE TABLE Course(
+	CourseID VARCHAR(15),
+	CourseName VARCHAR(30),
+	CourseBeforeID VARCHAR(15)
+);
+
+#21
+CREATE TABLE Choose(
+	SNO VARCHAR(20),
+	CourseID VARCHAR(30),
+	Score DECIMAL(5,2)
+);
+
+#22
+INSERT INTO Student(SNO, NAME, age, College) VALUES
+('s00001','张三',20,'计算机学院'),
+('s00002','李四',19,'通信学院'),
+('s00003','王五',21,'计算机学院');
+SELECT * FROM Student;
+
+#23
+INSERT INTO Course(CourseID, CourseName, CourseBeforeID) VALUES
+('C1','计算机引论',NULL),
+('C2','C语言','C1'),
+('C3','数据结构','C2');
+SELECT * FROM Course;
+
+#24
+INSERT INTO Choose(SNO, CourseID, Score) VALUES
+('S00001','C1','95'),
+('S00001','C2','80'),
+('S00001','C3','84'),
+('S00002','C1','80'),
+('S00002','C2','85'),
+('S00003','C1','78'),
+('S00003','C3','70');
+
+#25
+SELECT SNO,NAME FROM Student WHERE College = '计算机学院';
+
+#26
+SELECT * FROM Student WHERE age BETWEEN 20 AND 23;
+
+#27
+SELECT COUNT(*) FROM Student;
+
+#28
+SELECT MAX(Score) FROM Choose WHERE CourseID = 'c1';
+SELECT MIN(Score) FROM Choose WHERE CourseID = 'c1';
+SELECT SUM(Score) FROM Choose WHERE CourseID = 'c1';
+SELECT AVG(Score) FROM Choose WHERE CourseID = 'c1';
+
+#29
+SELECT CourseID, CourseName FROM Course WHERE CourseBeforeID IS NULL;
